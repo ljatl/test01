9a59d48fcc924d4411f09d9d3778676d036db17c
linjicdc <xhi6@cdc.gov>
2016-05-04 20:06:55 -0400

    Added files via upload

---
+++ image2.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.misc import imread, imresize
+import matplotlib.pyplot as plt
+
+import os
+file_path =  os.path.dirname(os.path.realpath(__file__))
+os.chdir(file_path)
+print os.getcwd()
+
+
+img = imread('cat.jpg')
+img_tinted = img * [1, 0.95, 0.9]
+
+# Show the original image
+plt.subplot(1, 2, 1)
+plt.imshow(img)
+
+# Show the tinted image
+plt.subplot(1, 2, 2)
+
+# A slight gotcha with imshow is that it might give strange results
+# if presented with data that is not uint8. To work around this, we
+# explicitly cast the image to uint8 before displaying it.
+plt.imshow(np.uint8(img_tinted))
+plt.show()
